f29760b34135ad31db1f06ebcfb2699092a35ed5
Gkolman <Gkolman17@gmail.com>
2020-12-02 19:18:16 -0800

    Complete connecting Video List and Player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 
 var App = () => (
 
@@ -11,7 +12,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em> <VideoPlayer video = {exampleVideoData[0]}/> </h5></div>
       </div>
       <div className="col-md-5">
         <div><h5><em>videoList</em> <VideoList videos = {exampleVideoData} /></h5></div>
