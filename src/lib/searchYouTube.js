bae628404f6a72ba569c4d85cc9e4ef8dfb79222
Gkolman <Gkolman17@gmail.com>
2020-12-05 11:29:51 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -2,35 +2,23 @@ var searchYouTube = (options, callback) => {
   // TODO
 
 
-  var request = $.ajax({
-    cache: false,
-    data: $.extend({
-      key: options.key,
-      part: 'snippet',
-      q: options.query
-    }, {
-      maxResults: options.max,
-    }),
-    dataType: 'json',
-    type: 'GET',
-    timeout: 1999,
-    url: 'https://www.googleapis.com/youtube/v3/search'
-  });
-
-  callback(request.responseJSON.items);
-
-  return request;
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    key: options.key,
+    part: 'snippet',
+    q: options.query,
+    maxResults: options.max,
+    type: 'video',
+    videoEmbeddable: true
+  })
+    .done(function(data) {
+      console.log('Invoke callback here',data);
+      callback(data.items);
+    })
+    .fail(function(err) {
+      console.log('something went bad', err);
+    });
 };
 
-var optionsExample = {
-  key: 'AIzaSyBNITgGM98eXt4iZyLo5ajv5zVZF0ng0BE',
-  max: '5',
-  query: 'react'
-};
-// var callBackExample = (data) => {
-//   expect(hasSameShape(data, exampleVideoData)).to.be.true;
-//   done();
-// };
-console.log('searchYouTube -> ', searchYouTube(optionsExample));
+
 
 export default searchYouTube;
